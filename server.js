fd1b3741d9b5bd17b15ad1b2ed4a5f151a8ca82c
Tasneem <tasneem.jameel9@gmail.com>
2024-02-27 13:01:43 +0300

    Install cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 require('dotenv').config()
 require('./config/database')
+const cors = require('cors')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -12,10 +13,12 @@ const authRouter = require('./routes/AuthRoutes')
 
 var app = express()
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
