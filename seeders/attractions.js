2548215f7ada1c213a0a03575ca7b700041c739c
Yasmeen <yomalbalooshi@gmail.com>
2024-02-28 10:44:02 +0300

    seeders

--- seeders/attractions.js
+++ seeders/attractions.js
@@ -21,55 +21,62 @@ const createAttractions = async () => {
     if (zone.name === 'Splash Kingdom') {
       attractions = [
         {
-          name: 'SplashDown Falls',
+          name: 'Surf n Dine',
           description:
-            'A thrilling water slide that twists and turns before plunging into a refreshing pool below',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            "Dive into a culinary oasis at our water theme park's eating zone, where a delectable fusion of flavors meets the refreshing ambiance of water-side dining, creating a feast for the senses.",
+          imageUrl: '/images/blue-1.png',
+          x: 95,
+          y: 195,
+          r: 27
         },
         {
           name: 'WaveRunner Rapids',
           description:
             'Experience the excitement of riding the rapids on inflatable rafts through a series of splashing waves and currents.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+          imageUrl: '/images/blue3.png',
+          x: 390,
+          y: 325,
+          r: 27
         },
         {
-          name: 'WaterWhirl Cove',
+          name: 'Aqua Arena',
           description:
-            ' family-friendly water playground with slides, fountains, and water cannons, perfect for kids of all ages to splash and play.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Make a splash and spike up the fun at our pool volleyball arena, where sun-soaked excitement and friendly competition collide for an exhilarating aquatic experience.',
+          imageUrl: '/images/blue-2.png',
+          x: 387,
+          y: 145,
+          r: 27
         }
       ]
     } else if (zone.name === 'Kidtopia') {
       attractions = [
         {
-          name: `Tiny Explorers' Trail`,
+          name: `Bounceville Wonderland`,
           description:
-            'A whimsical walkway filled with interactive play areas, perfect for sparking the imagination of young adventurers.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Welcome to Bounceville Wonderland in Kidtopia, where laughter fills the air as children revel in the joyous magic of our vibrant jumping castle, creating unforgettable moments of boundless fun and adventure.',
+          imageUrl: '/images/green-1.png',
+          x: 47,
+          y: 230,
+          r: 27
         },
         {
-          name: 'Kiddie Carousel Cove',
+          name: 'JumpQuack Oasis',
           description:
-            'A charming carousel featuring colorful, animal-shaped seats that spin gently for a delightful ride.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Step into the whimsical JumpQuack Oasis at Kidtopia, where the air is filled with giggles and quacks as children embark on a lively escapade of bouncing delight with our charming jumping ducks, creating a buoyant playground of laughter and playful excitement.',
+          imageUrl: '/images/green-2.png',
+          x: 340,
+          y: 180,
+          r: 27
         },
         {
-          name: 'FunZone Playland',
+          name: 'CircusFiesta Junction',
           description:
-            'An indoor play area filled with climbing structures, ball pits, and slides, providing hours of fun for energetic kids.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Welcome to CircusFiesta Junction in Kidtopia, where the air is filled with the joyful juggling antics of our whimsical clowns, creating a vibrant spectacle of laughter and merriment for all to enjoy.',
+          imageUrl: '/images/green-3png.png',
+
+          x: 343,
+          y: 360,
+          r: 27
         }
       ]
     } else if (zone.name === 'Kingdom of Dreams') {
@@ -78,52 +85,58 @@ const createAttractions = async () => {
           name: `Dragon's Lair Quest`,
           description:
             'Embark on a thrilling adventure through a medieval castle, complete with interactive challenges and encounters with mythical creatures.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+          imageUrl: '/images/yellow-1.png',
+          x: 335,
+          y: 400,
+          r: 19
         },
         {
-          name: `Knight's Valor Arena`,
+          name: `Ethereal Spin Kingdom`,
           description:
-            'Witness epic battles and daring feats of skill as knights compete in jousting tournaments and sword fights.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Embark on a captivating journey at the Ethereal Spin Kingdom in the Kingdom of Dreams Zone, where the grandeur of the Ferris Wheel unveils a panoramic spectacle, offering breathtaking views that transport visitors into a realm of enchantment, delight, and suspended time.',
+          imageUrl: '/images/yellow2.png',
+          x: 385,
+          y: 130,
+          r: 27
         },
         {
-          name: 'Royal Jousting Challenge',
+          name: 'ThrillRush Summit',
           description:
-            'Test your skills on horseback as you joust against opponents in a medieval tournament of champions.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Prepare for an adrenaline-fueled adventure at ThrillRush Summit in the Kingdom of Dreams Zone, where the dynamic roller coaster takes center stage. With heart-pounding drops, exhilarating loops, and breathtaking twists, this roller coaster promises an electrifying ride through a realm of excitement and pulse-pounding thrills, creating an unforgettable experience for daredevils and thrill-seekers alike.',
+          imageUrl: '/images/yellow3.png',
+          x: 389,
+          y: 310,
+          r: 27
         }
       ]
     } else if (zone.name === 'Tropical Tides') {
       attractions = [
         {
-          name: `Seaside Serenity Spa`,
+          name: `IslandExplorer Odyssey`,
           description:
-            'Relax and rejuvenate with a variety of spa treatments, from massages to facials, all while enjoying panoramic views of the beach.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Explore the tropical wonders of Tropical Tides on the IslandExplorer Odyssey, a serene train ride through lush landscapes and coastal vistas, offering a relaxing journey amidst island beauty.',
+          imageUrl: '/images/pink-1.png',
+          x: 85,
+          y: 245,
+          r: 27
         },
         {
-          name: `Surf's Up Wave Pool`,
+          name: `TropicTwist Coaster`,
           description:
-            'Experience the thrill of surfing on artificial waves in a controlled environment, perfect for surfers of all skill levels.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Experience the rush of TropicTwist Coaster in Tropical Tides, where exhilarating loops and twists blend seamlessly with breathtaking ocean vistas, offering a thrilling ride through a tropical paradise.',
+          imageUrl: '/images/pink-2.png',
+          x: 375,
+          y: 195,
+          r: 27
         },
         {
-          name: 'Royal Jousting Challenge',
+          name: 'AquaBump Dash',
           description:
-            'Join in on beach volleyball tournaments, sandcastle building contests, and other fun beach activities.',
-          imageUrl: '',
-          x: 100,
-          y: 100
+            'Dive into the fun-filled AquaBump Dash in Tropical Tides, where the excitement of bumper cars meets a refreshing aquatic twist. Navigate the watery arena, gleefully bumping and dodging amidst tropical surroundings, creating a splashing good time for all ages.',
+          imageUrl: '/images/pink-3.png',
+          x: 380,
+          y: 376,
+          r: 27
         }
       ]
     }
--- seeders/zones.js
+++ seeders/zones.js
@@ -17,33 +17,45 @@ const createZones = async () => {
       name: 'Splash Kingdom',
       openingTime: '8:00',
       closingTime: '16:00',
-      x: 100,
-      y: 100,
-      price: 10
+      image: '/images/bluemap.svg',
+      fastLane: 20,
+      normal: 10,
+      x: 365,
+      y: 173,
+      r: 19
     },
     {
       name: 'Kidtopia',
       openingTime: '8:00',
       closingTime: '16:00',
-      x: 100,
-      y: 100,
-      price: 20
+      image: '/images/greenmap.svg',
+      fastLane: 30,
+      normal: 15,
+      x: 34,
+      y: 235,
+      r: 19
     },
     {
       name: 'Kingdom of Dreams',
       openingTime: '8:00',
       closingTime: '16:00',
-      x: 100,
-      y: 100,
-      price: 30
+      image: '/images/yellowmap.svg',
+      fastLane: 25,
+      normal: 15,
+      x: 335,
+      y: 400,
+      r: 19
     },
     {
       name: 'Tropical Tides',
       openingTime: '8:00',
       closingTime: '16:00',
-      x: 100,
-      y: 100,
-      price: 40
+      image: '/images/pinkmap.svg',
+      fastLane: 30,
+      normal: 20,
+      x: 403,
+      y: 616,
+      r: 19
     }
   ]
 
